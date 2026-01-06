a7a3d28ab11840b9e2cb58a1c8d060930b4eaa33
Piyush6678 <aggarwalpiyush6678@gmail.com>
2026-01-06 21:00:12 +0530

    added backend url

--- src/Helpers/axiosinstace.js
+++ src/Helpers/axiosinstace.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.VITE_BACKEND_URL
+const BASE_URL = "https://lms-server-yzbd.onrender.com/api/v1";
 
 
 const axiosInstance =axios.create();
