66aabb53ef343ce34926ac4b15de803e1b3ce5d0
Piyush6678 <aggarwalpiyush6678@gmail.com>
2026-01-06 21:06:53 +0530

    remove backend url

--- src/Helpers/axiosinstace.js
+++ src/Helpers/axiosinstace.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-server-yzbd.onrender.com/api/v1";
+const BASE_URL = process.env.VITE_BACKEND_URL;
 
 
 const axiosInstance =axios.create();
